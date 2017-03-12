29acd1beeeabfd71499f6d42bc711bca28072298
Tanya Powell <hello@tanyapowell.co.uk>
2017-03-12 23:06:15 +0000

    table texture addded to background

--- src/displayobjects/Background/Background.js
+++ src/displayobjects/Background/Background.js
@@ -1,12 +1,19 @@
 import PIXI from 'pixi.js';
-import TEXTURE from '../images/kitchen.png';
+
+import { config } from '../../../package.json';
+import BACKGROUND_TEXTURE from '../images/kitchen.png';
+import TABLE_TEXTURE from '../images/table.png';
 
 export default class Background extends PIXI.Container {
 
   constructor() {
     super();
 
-    const bg = PIXI.Sprite.fromImage(TEXTURE);
+    const positionX = config.stageWidth / 3.5;
+    const positionY = config.stageHeight / 5;
+
+    const bg = PIXI.Sprite.fromImage(BACKGROUND_TEXTURE);
+    const table = PIXI.Sprite.fromImage(TABLE_TEXTURE);
 
     bg.anchor.x = 0;
     bg.anchor.y = 0;
@@ -14,11 +21,16 @@ export default class Background extends PIXI.Container {
     bg.position.x = -120;
     bg.position.y = 50;
 
+    table.x = positionX + 230;
+    table.y = positionY + 420;
+    table.scale.x = 1.5;
+    table.scale.y = 1.5;
+
     const blurFilter = new PIXI.filters.BlurFilter();
     blurFilter.blur = 3;
     bg.filters = [blurFilter];
 
-    this.addChild(bg);
+    this.addChild(bg, table);
   }
 
 }
