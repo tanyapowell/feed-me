8603ac985fca673e9a3b04681a6b6aa3ac519368
Tanya Powell <hello@tanyapowell.co.uk>
2017-03-12 23:03:23 +0000

    animation store removed

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 import './index.html';
-import {config} from '../package.json';
+import { config } from '../package.json';
+import framesCounter from './utils/fps/framesCounter';
 import Renderer from './Renderer/Renderer';
 import App from './displayobjects/App/App';
-import AnimationStore from './stores/AnimationStore';
-import TWEEN from 'tween.js';
+import GameStore from './stores/GameStore';
 
 const renderer = new Renderer(config.stageWidth, config.stageHeight);
 const app = new App(config.stageWidth, config.stageHeight);
 
 document.body.appendChild(renderer.view);
 
-AnimationStore.addChangeListener(() => TWEEN.update());
+GameStore.set('gameOutcome', GameStore.setGameOutcome());
 
 renderer.addRenderable(app);
 renderer.start();
+framesCounter();
